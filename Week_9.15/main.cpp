1487c09aadd7da193f05f3f1a36dc1714c49f3aa
Young22li <121653561+Young22li@users.noreply.github.com>
2024-09-20 21:42:47 -0400

    Create main.cpp

---
+++ Week_9.15/main.cpp
@@ -0,0 +1,9 @@
+//Identifying pixels
+//Edit By: Ziyang Li
+//Date: 9/20/24
+//Description：Identifying pixels (in a 2D image) corresponding to X-rays in four directions (horizontal, vertical, diagonal, and anti-diagonal)
+
+#include <iostream>
+using namespace std;
+
+int main(){}
