442262225670bcc0460a90e3b11a2d7bcf696d10
Young22li <121653561+Young22li@users.noreply.github.com>
2024-09-23 11:09:04 -0400

    Update main.cpp

--- Week_9.15/main.cpp
+++ Week_9.15/main.cpp
@@ -7,19 +7,93 @@
 using namespace std;
 
 const int n = 10;
+//Try arrays of different sizes
 //const int n = 20;
+//const int n = 8;
+
+void line2(int x1, int y1, int x2, int y2, int ray, short (&P)[n][n])//Using Array References
+{
+    int x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+    dx = x2 - x1;
+    dy = y2 - y1;
+    dx1 = abs(dx);
+    dy1 = abs(dy);
+    px = 2 * dy1 - dx1;
+    py = 2 * dx1 - dy1;
+    if (dy1 <= dx1)
+    {
+        if (dx >= 0)
+        {
+            x = x1;
+            y = y1;
+            xe = x2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            xe = x1;
+        }
+        P[x][y] = ray;
+        for (i = 0; x < xe; i++)
+        {
+            x = x + 1;
+            if (px < 0) px = px + 2 * dy1;
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) y = y + 1;
+                else y = y - 1;
+            
+                px = px + 2 * (dy1 - dx1);
+            }
+
+            P[x][y] = ray;
+        }
+    }
+    else
+    {
+        if (dy >= 0)
+        {
+            x = x1;
+            y = y1;
+            ye = y2;
+        }
+        else
+        {
+            x = x2;
+            y = y2;
+            ye = y1;
+        }
+        P[x][y] = ray;
+        for (i = 0; y < ye; i++)
+        {
+            y = y + 1;
+            if (py <= 0) py = py + 2 * dx1;
+            else
+            {
+                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) x = x + 1;
+            
+                else x = x - 1;
+                
+                py = py + 2 * (dx1 - dy1);
+            }
+
+            P[x][y] = ray;
+        }
+    }
+}
 
 
 int main() {
-  //short verticalRays[n][n];
-  //short horizontalRays[n][n];
+  short verticalRays[n][n];
+  short horizontalRays[n][n];
   short diagonalRays[n][n];
   short antiDiagonalRays[n][n];
   int row, col;
   short ray;
 
   //Computing vertical rays
-  /*
+  
   for (row = 0; row < n; row++) {
     ray = 11;
     for (col = 0; col < n; col++) {
@@ -36,7 +110,7 @@ int main() {
     }		
     ray++;
   }
-  */
+
 
   //Computing diagonal rays
   ray = 11;
@@ -70,7 +144,7 @@ int main() {
   //**********************************
 
   //Displaying vertical rays
-  /*
+
   cout << endl << "Vertical rays:" << endl;
   for (row = 0; row < n; row++) {
     for (col = 0; col < n; col++)
@@ -85,7 +159,7 @@ int main() {
       cout << horizontalRays[row][col] << " ";
     cout << endl;
   }
-  */
+
 
   //Displaying diagonal rays
   cout << endl << "diagonal rays:" << endl;
