3196c0e3a73c9fb78c53b33546c91cd3f4a8b025
Young22li <121653561+Young22li@users.noreply.github.com>
2024-11-25 11:41:51 -0500

    Update main.cpp

--- Week_11.10/main.cpp
+++ Week_11.10/main.cpp
@@ -1,8 +1,12 @@
+/*************
+    Name: Ziyang Li
+    Update Date: 11/25/2024 
+**************/
+
 #include <iostream>
 #include "Display.h"
 #include <cmath>
 #include <algorithm>
-#include "line_functions.h"
 using namespace std;
 
 const int n = 180; // Image Size N x N
@@ -16,39 +20,6 @@ int final[n][n] = {0};
 int sinogram[n][n];
 
 int sino[n][n];
-
-
-
-void square(int row1, int row2, int col1, int col2)
-{
-    for (int i = row1 - 1; i < row2; i++)
-    {
-        for (int j = col1 - 1; j < col2; j++)
-            Pixels[i][j] = 250;
-    }
-}
-
-void circle(int x1, int y1, int r)
-{
-    for (int x = x1 - r; x < r + x1; x++)
-    {
-        int dx = x - x1;
-        int right = r * r - dx * dx;
-        if (right >= 0)
-        {
-            int y = sqrt(right);
-            line3(x, y + y1, x, y1 - y, Pixels);
-        }
-    }
-}
-
-void trangle(int h1, int h2, int b1, int b2, int bRow)
-{
-    for (int i = b1; i < b2; i++)
-    {
-        line3(h1, h2, bRow, i, Pixels);
-    }
-}
 
 void rotation(int angle, int (&Pixels)[n][n])
 {
@@ -165,66 +136,12 @@ void sp2(){
 
 int main()
 {
-
-    for (int row = 0; row < n; row++)
-    {
-        for (int col = 0; col < n; col++)
-        {
-            Pixels[row][col] = 2;
-        }
-    }
-
-    // Creat three shape
-    square(23, 65, 33, 65);
-    circle(88, 88, 20);
-    trangle(100, 123, 100, 150, 150);
-
-    Display image(Pixels, "output1");
-
-
-    for (int i = 0; i < n; i++)
-    {
-        rotation(i, Pixels);
-        for (int row = 0; row < n; row++)
-        {
-            int res = 0;
-
-            for (int col = 0; col < n; col++)
-            {
-                res += tracy[col][row];
-            }
-            sinogram[i][row] = res;
-        }
-    }
-
-    reduce(sinogram);
-    
-    Display image3(sinogram, "output2");
-
-
-    //for(int i = 0; i < n; i++) re(i, sinogram);
-    
     //for(int i = 0; i < n; i++)  sp(i);
-
-    sp2();
-    
+    sp2(); 
     reduce(sino);
-    Display imagesss(sino, "output6");
-    /*
-    for(int i = 0; i < n; i++){
-        cout<<sinogram[0][i]<<" ";
-    }
-    cout<<endl<<endl<<endl;
-    for(int i = 0; i < n; i++){
-        cout<<sino[120][i]<<" ";
-    }
-    */
-
+    
     for(int i = 0; i < n; i++) re(i, sino);
-
-
     reduce(final);
-
     Display image2(final, "output");
 
     return 0;
